f465c8cb6828dc25407c369665956b30d901eceb
RuxandraRambet <ruxandraramb@gmail.com>
2023-11-10 10:43:20 +0100

    fix: consolelog

--- esercizio2/main.js
+++ esercizio2/main.js
@@ -13,8 +13,13 @@ let winnerElement = document.querySelector('.winner');
 
 if (userNumber === computerNumber) {
     winnerElement.innerHTML = "It's a tie";
+    console.log( "It's a tie");
 }else if ( userNumber > computerNumber) {
     winnerElement.innerHTML = " Human player wins";
+    console.log( " Human player wins");
 }else {
     winnerElement.innerHTML = "Computer wins"
+    console.log( "Computer wins");
 }
+
+
